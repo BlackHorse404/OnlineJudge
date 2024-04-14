54dce63d6d4525d2e403de9e3536530b6225de0c
phatlino <phatlongtoan@gmail.com>
2024-04-14 21:27:36 +0700

    push code

---
+++ OnlineJudge_BE/conf/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+
+
+class JudgeServer(models.Model):
+    hostname = models.TextField()
+    ip = models.TextField(null=True)
+    judger_version = models.TextField()
+    cpu_core = models.IntegerField()
+    memory_usage = models.FloatField()
+    cpu_usage = models.FloatField()
+    last_heartbeat = models.DateTimeField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    task_number = models.IntegerField(default=0)
+    service_url = models.TextField(null=True)
+    is_disabled = models.BooleanField(default=False)
+
+    @property
+    def status(self):
+        # Thêm độ trễ một giây để cải thiện khả năng thích ứng với môi trường mạng
+        if (timezone.now() - self.last_heartbeat).total_seconds() > 6:
+            return "abnormal"
+        return "normal"
+
+    class Meta:
+        db_table = "judge_server"
